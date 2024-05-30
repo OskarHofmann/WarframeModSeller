5adb94deaaa88403da2d161127d2bfec1fd15632
Oskar Hofmann <oskar.hofmann@gmx.de>
2024-05-30 23:23:21 +0200

    Implement repeating for 200 response.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import aiohttp
-import requests
 import pandas as pd
 import asyncio, aiohttp
 # from dotenv import load_dotenv
@@ -36,37 +34,42 @@ def mod_names_to_ids(mods: list[str]) -> list[str]:
 #     return [mod.replace('_', ' ').title() for mod in mods]
 
 
-async def get_order_prices(item_id: str, session: aiohttp.ClientSession) -> list[int] :
+async def get_order_prices(item_id: str, session: aiohttp.ClientSession, n_tries: int = 10) -> list[int] :
     api_url = MARKET_URL + f'/items/{item_id}/orders'
-    async with session.get(api_url) as response:
-        if response.status != 200:
-            print(f'API call for {item_id} failed.')
-            return []
-        response_json = await response.json(content_type=None)
-        try:
-            orders = response_json['payload']['orders']
-        except KeyError:
-            print(response_json)
-        available_orders = [order for order in orders if \
-                            order['user']['status'] == 'ingame' and \
-                            order['order_type'] == 'sell']
-        prices = [order['platinum'] for order in available_orders]
-        prices.sort()
-        return prices
+    # try n_tries times, before giving up
+    for _ in range(n_tries):
+        async with session.get(api_url) as response:
+            if response.status != 200:
+                await asyncio.sleep(1)
+                continue
+            response_json = await response.json(content_type=None)
+
+        orders = response_json['payload']['orders']
+        break
+    else:
+        print(f'API call for {item_id} failed.')
+        return []
+
+    available_orders = [order for order in orders if \
+                        order['user']['status'] == 'ingame' and \
+                        order['order_type'] == 'sell']
+    prices = [order['platinum'] for order in available_orders]
+    prices.sort()
+    return prices
 
 
 async def get_mod_prices(mods: list[str]) -> dict[str, list[int]]:
-    mod_prices = {}
     mod_ids = mod_names_to_ids(mods)
     async with aiohttp.ClientSession() as session:
         tasks = [get_order_prices(mod_id, session) for mod_id in mod_ids]
+        print('Gathering mod prices from WarframeMarket. Please wait.')
         results = await asyncio.gather(*tasks)          
 
-    # gather returns in same order as in the task order (i.e. the same order as in mod_ids)  :
+    # gather returns in same order as in the task order (i.e. the same order as in mod_ids)
     return dict(zip(mod_ids, results))
 
 
 if __name__ == '__main__':
     # load_dotenv()
     mods = get_augment_mods('Red Veil')
-    print(asyncio.run(get_mod_prices(mods)))
+    mod_prices = asyncio.run(get_mod_prices(mods))
