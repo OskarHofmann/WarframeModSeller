d687390fc8680bee081538e6569d9a7bde79d172
Oskar Hofmann <oskar.hofmann@gmx.de>
2024-06-01 21:14:01 +0200

    Remove augment_mods() from main

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-import pandas as pd
 import asyncio, aiohttp
 # from dotenv import load_dotenv
 # import unicodedata
 
 import parameters as params
+import augment_mods
 
-
-
-
-def get_augment_mods(syndicate: str | None = None, replace_special_whitespaces: bool = True) -> list[str]:
-    df = pd.read_html(params.AUGMENT_MODS_URL)[0]
-
-    # the DataFrame contains all mods seperated by spaces (individual words of a mod are split via non-break space \xa0 ) 
-    augment_mods = []
-    for idx, row in df.iterrows():
-        # check if row is for given syndicate (row contains name with non-break space \xa0)
-        if syndicate and not (syndicate.replace(' ', '\xa0') in row['Favored Syndicates']):
-            continue
-        mod_words = row['Augment Mods'].split(' ')
-        augment_mods.extend(mod_words)
-
-    if replace_special_whitespaces:
-        # replace non-break space with normal spaces (normalized equivalent)
-        # augment_mods = [unicodedata.normalize('NFKD', mod) for mod in augment_mods]
-        augment_mods = [mod.replace('\xa0', ' ') for mod in augment_mods]
-    
-    return augment_mods
 
 
 def mod_names_to_ids(mods: list[str]) -> list[str]:
@@ -72,7 +51,7 @@ async def get_mod_prices(mods: list[str]) -> dict[str, list[int]]:
 
 if __name__ == '__main__':
     # load_dotenv()
-    mods = get_augment_mods('Red Veil')
+    mods = augment_mods.get_augment_mods('Red Veil')
     mod_prices = asyncio.run(get_mod_prices(mods))
 
     cheapest_offers = {name: prices[0] for name, prices in mod_prices.items() if len(prices) > 0}
