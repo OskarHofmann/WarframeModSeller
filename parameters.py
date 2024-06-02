925eb4787342bdb7614adebca5177f23e58f82d3
Oskar Hofmann <oskar.hofmann@gmx.de>
2024-06-02 15:24:32 +0200

    Add additional parameters

--- parameters.py
+++ parameters.py
@@ -2,4 +2,4 @@ MARKET_URL = 'https://api.warframe.market/v1'
 AUGMENT_MODS_URL = 'https://warframe.fandom.com/wiki/Warframe_Augment_Mods/PvE'
 
 SECONDS_BEFORE_RETRY_API_CALL = 1
-NUMBER_OF_API_CALL_RETRIES = 100
+NUMBER_OF_API_CALL_RETRIES = 20
