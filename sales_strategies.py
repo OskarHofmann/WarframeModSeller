3d396c73da7ba02d183f6b5853e3e6958106e8c3
Oskar Hofmann <oskar.hofmann@gmx.de>
2024-06-09 21:12:01 +0200

    Implement and update SalesStrategies

--- sales_strategies.py
+++ sales_strategies.py
@@ -21,14 +21,15 @@ class SellAllAtPrice(SalesStrategy):
     @staticmethod
     def propose_mods_to_sell(market_items: MarketItems, 
                              sell_price: int = 0, 
-                             sell_at_highest = True,
-                             difference_to_highest = 0
+                             sell_at_highest: bool = True,
+                             difference_to_highest: int = 0
                              ) -> list[tuple[MarketItem, int]]:
         
         if sell_at_highest:
-            cheapest_offers = super(SellAllAtPrice, SellAllAtPrice).get_cheapest_offers(market_items)
+            cheapest_offers = super(SellAllAtPrice, SellAllAtPrice).get_cheapest_offers(market_items) # two-argument super required due to acces to static method
             highest_price = max([price for _,price in cheapest_offers])
-            return [(item, highest_price) for item in market_items.items]
+            price_to_sell_at = highest_price - difference_to_highest
+            return [(item, price_to_sell_at) for item in market_items.items]
 
         elif sell_price > 0:
             return [(item, sell_price) for item in market_items.items]
@@ -41,4 +42,19 @@ class SellAllAtPrice(SalesStrategy):
 
 
 class SellMostProfitable(SalesStrategy):
-    pass
+    
+    @staticmethod
+    def propose_mods_to_sell(market_items: MarketItems, 
+                             difference_to_highest: int = 0, 
+                             sell_below_current_cheapest: bool = False
+                             ) -> list[tuple[MarketItem, int]]:
+    
+        cheapest_offers = super(SellAllAtPrice, SellAllAtPrice).get_cheapest_offers(market_items) # two-argument super required due to acces to static method
+        highest_price = max([price for _,price in cheapest_offers])
+
+        items_to_sell = [(item, price - 1 * sell_below_current_cheapest) 
+                         for (item, price) in cheapest_offers 
+                         if price >= (highest_price - difference_to_highest)]
+        
+        return items_to_sell
+
