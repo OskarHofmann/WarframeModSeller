597e2ae95c9d1a78d2379bf848a558fde865fc38
Oskar Hofmann <oskar.hofmann@gmx.de>
2024-06-16 16:20:38 +0200

    Implement user login

--- libs/authentification.py
+++ libs/authentification.py
@@ -1,54 +1,29 @@
 import parameters as params
 import requests
 
-from dataclasses import dataclass
-
-@dataclass
-class WFMarketUser():
-    email: str = ""
-    password: str = ""
-    name: str = ""
-    id: str = ""
 
 class WFMarketAuth():
     
-    def __init__(self, email:str, password: str) -> None:
-        self.user = WFMarketUser(email=email, password=password)
-
+    def __init__(self) -> None:
         #get JSON web token (JWT) from WarframeMarket for API calls that require user authentification
-        jwt_request = requests.get(api_url)
+        jwt_request = requests.get(params.MARKET_URL)
         self.jwt = jwt_request.cookies["JWT"]
 
 
-
-# for testing
-if __name__ == '__main__':
-    import sys
-    sys.path.insert(0, '.')
-    
-    import parameters as params
-    import os
-    from dotenv import load_dotenv
-    import requests
-
-    load_dotenv()
-    wf_market_email = os.environ['WFM_EMAIL']
-    wf_market_password = os.environ['WFM_PASSWORD']
-
-    api_url = params.MARKET_URL + '/auth/signin'
-
-    jwt_request = requests.get(api_url)
-    jwt = jwt_request.cookies["JWT"]
-
-
-    data = {
-        "auth_type": "header",
-        "email": wf_market_email,
-        "password": wf_market_password
+    def login_user(self, email: str, password:str) -> None:
+        signin_url = params.MARKET_URL + '/auth/signin'
+        login_data = {
+            "auth_type": "header",
+            "email": email,
+            "password": password
         }
-    header = {
-        "Authorization": jwt
-    }
-    
-    response = requests.post(api_url, json=data, headers=header)
-    print(response.text)
+        login_header = {"Authorization": self.jwt}
+
+        response = requests.post(signin_url, json=login_data, headers=login_header)
+        
+        if response.status_code != 200:
+            raise requests.ConnectionError("Could not login with provided credentials. Please verify credentials or check if WarframeMarket.com is available.")
+        
+        user_data = response.json()["payload"]["user"]
+        self.user_id = user_data["id"]
+        self.user_name = user_data["ingame_name"]
