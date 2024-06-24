9331f348dc53ace78db1d45bb81e74b4ac4264c2
Oskar Hofmann <oskar.hofmann@gmx.de>
2024-06-24 23:42:50 +0200

    Update orders

--- libs/orders.py
+++ libs/orders.py
@@ -1,6 +1,20 @@
+from dataclasses import dataclass
 from libs.authentification import WFMarketAuth
+from libs.market_items import MarketItem
 import parameters as params
 import requests
+
+
+@dataclass
+class MarketOrder():
+    full_order_info: dict
+    id: str = ""
+    item: MarketItem | None = None
+
+    # read out id and item info from full_order_info dict/json
+    def __post_init__(self) -> None:
+        pass
+
 
 
 def get_current_user_sell_orders(auth: WFMarketAuth) -> list[dict]:
@@ -8,16 +22,15 @@ def get_current_user_sell_orders(auth: WFMarketAuth) -> list[dict]:
     header = auth.get_auth_header()
 
     response = requests.get(api_url, headers = header)
-    
     return response.json()["payload"]["sell_orders"]
 
 
-def delete_order():
+def delete_order(order: MarketOrder, auth: WFMarketAuth):
     pass
     
-#TODO: Move functions to seperate class
-def create_order():
+
+def create_order(item: MarketItem, auth: WFMarketAuth):
     pass
 
-def update_order():
+def update_order(order: MarketOrder, auth: WFMarketAuth, new_price: int):
     pass
